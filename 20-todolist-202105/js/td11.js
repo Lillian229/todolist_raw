05ae01bd43df16ba0fc3431c893ef04f4186b1f4
yaya <leelillian402@gmail.com>
2021-10-21 20:25:52 +0800

    💤10/21

---
+++ 20-todolist-202105/js/td11.js
@@ -0,0 +1,91 @@
+const key = 'he_v1.11'
+
+function readData() {
+    let data = window.localStorage.getItem(key)
+    data = JSON.parse(data)
+    !data && (data= [])
+    return data
+
+}
+
+function saveData(data) {
+    data = JSON.stringify(data)
+    localStorage.setItem(key,data)
+}
+
+$(function() {
+    let $todo = $('#todolist')
+    let $done = $('#donelist')
+    let $todoNum = $('#todocount')
+    let $doneNum = $('#donecount')
+    let $title = $('#title')
+    let data = readData()
+
+    load()
+
+    function load() {
+        data.forEach(t => {
+            addTask(t)
+        })
+        count()
+    }
+
+    function addTask(task) {
+        let $li = $(`<li>
+                    <input type = "checkbox" data-id = "${task.id}"
+                    <p> ${task.title} </p>
+                    <a href = "javascript:;" data-id = "${task.id}"></a>
+                    </li>`)
+        if(task.done) {
+            $li.children('input').prop('checked',true)
+            $done.prepend($li)
+        } else {
+            $todo.prepend($li)
+        }
+    }
+
+    $('#todolist,#donelist').on('change','li>input[type = checkbox]',function() {
+        let $li = $(this).parent()
+        if(this.checked) $done.prepend($li)
+        else $todo.prepend($li)
+
+        let target = data.find(e => e.id == this.dataset.id)
+        target && (target.done = !target.done)
+        saveData(data)
+        count()
+
+    })
+
+    $('#title').on('keyup',function (e) {
+        if(e.key != "Enter") return
+        let newTask = {
+            id:data.length === 0 ? 1 :data[data.length - 1].id + 1,
+            title:this.value.trim(),
+            done:false
+        }
+
+    addTask(newTask)
+
+    data.push(newTask)
+    saveData(data)
+
+    count()
+    })
+
+
+    $('#todolist,#donelist').on('click','li>a',function() {
+        let $li = $(this).parent()
+        $li.remove()
+
+        let i = data.findIndex(e => e.id ==this.dataset.id)
+        i > -1 && data.splice(i,1)
+        saveData(data)
+
+        count()
+    })
+
+    function count() {
+        $todoNum.text(data.filter(e => !e.done).length)
+        $doneNum.text(data.filter(e => e.done).length)
+    }
+})
